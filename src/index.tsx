bd041f3b906367ff60d7872e8cabbeb08b31f33a
adamgajzlerowicz <nelf86@gmail.com>
2021-04-30 10:01:27 +0200

    Added firebase connection

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Kanban} from "./kanban/kanban";
 import { createGlobalStyle } from 'styled-components'
+import firebase from 'firebase'
+
 import './index.css'
 
+firebase.initializeApp({
+    apiKey: "AIzaSyBY8dOhtotTEc8LblVNHxjW2Et2TFlj4UM",
+    authDomain: "tasks-app-demo-312219.firebaseapp.com",
+    projectId: "tasks-app-demo-312219",
+    storageBucket: "tasks-app-demo-312219.appspot.com",
+    messagingSenderId: "22392413963",
+    appId: "1:22392413963:web:fb36b0633f0d559d05cc0e"
+})
 
 const CSSReset = createGlobalStyle`
   * {
--- src/kanban/kanban.tsx
+++ src/kanban/kanban.tsx
@@ -1,20 +1,36 @@
 import * as React from 'react'
 import styled from "styled-components";
 import {colors, mediumSize} from "../design-system";
+import firebase from 'firebase'
 import {Lane} from "./components/lane";
+import { useCollection } from 'react-firebase-hooks/firestore';
 import {BoardItem } from "./types";
 
 const Container = styled.div`
-  padding: ${mediumSize}px;
+  padding-top: ${mediumSize}px;
+  padding-left: ${mediumSize}px;
+  padding-right: ${mediumSize}px;
   width: 100%;
   display: grid;
   grid-template-columns:repeat(4, 1fr);
   gap: ${mediumSize*2}px;
   background-color: ${colors.background};
-  min-height: 600px;
+  min-height: 100vh;
 `
 
 export const Kanban = () => {
+    const [data, loading, error] = useCollection(
+        firebase.firestore().collection('boards').doc('gmcMohbh9a4GSVFDivFv').collection('tasks'),
+        {
+            snapshotListenOptions: { includeMetadataChanges: true },
+        }
+    );
+
+    if (loading) {
+        return <div>loading</div>
+    }
+
+
     const items: Array<BoardItem> = [{
         title: "foo",
         lane: "candidates"
